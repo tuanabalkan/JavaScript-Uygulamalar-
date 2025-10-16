6a65b084f25cd558ea359df15f2fc88ccd3193c2
tuanabalkan <tuana.balkan@ogr.gelisim.edu.tr>
2025-10-16 11:56:12 +0300

    Add files via upload

---
+++ ciktivermeislemleri.js
@@ -0,0 +1,13 @@
+//console.log() - en temel mesaj verme yöntemi
+console.log("Tuana Balkan");
+console.log(55); //integer
+console.log(3.14); //float ya da double)
+
+//console.error() - hata mesajı
+console.error("bu bir hata mesajıdır");
+
+//console.warn() - uyarı mesajı
+console.warn("bu bir uyarı mesajıdır");
+
+//console.info() - bilgi mesajıdır
+console.info("bu bir bilgi mesajıdır");
